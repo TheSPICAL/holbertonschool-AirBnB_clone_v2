4722cefb844fdb3b3fc32d9c659bcfa28e334325
TheSPICAL <youssefmasakni@gmail.com>
2023-08-19 13:00:47 +0100

    create a flask file that returns the message Hello HBNB!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+ @app.route('/')
 def index():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+    
