57bc01afbb399db879704b2052ba788e3ef39b0d
TheSPICAL <youssefmasakni@gmail.com>
2023-08-19 13:31:00 +0100

    flask file that returns the message Hello HBNB!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-"""
-Hello HBNB! message shows on the localhost, port 5000
-"""
 
 from flask import Flask
 
+"""
+Hello HBNB! message shows on the localhost, port 5000.
+"""
+
 app = Flask(__name__)
 
- @app.route('/')
+
+@app.route('/')
 def index():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-    
