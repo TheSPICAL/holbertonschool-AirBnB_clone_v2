5037bb5c3801ab0ef4ffa08ce98db2d8af51278c
TheSPICAL <youssefmasakni@gmail.com>
2023-08-19 19:12:19 +0100

    adding another slash that returns the message HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+just testing for now
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB ():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host='0.0.0.0')
